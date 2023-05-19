b1bbb07f96c07813e22ffe52576a602d647619f2
Victor Owiti <viowiti12@gmail.com>
2023-05-19 15:08:09 +0300

    Output utils

---
+++ 1_hello_world.php
@@ -0,0 +1,11 @@
+<?php
+// Echo Outputs data, can take several args
+echo "Hello World" ,  "Second arg";
+
+// Print outputs data too, but takes only a single arg
+print "Hello from Print";
+
+// For single values and arrays
+print_r([1,2,3,4])
+
+?>
