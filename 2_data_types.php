a6952561a1275ca1af8d471e61a2fc8f411d239e
Victor Owiti <viowiti12@gmail.com>
2023-05-20 00:24:19 +0300

    Data types

---
+++ 2_data_types.php
@@ -0,0 +1,32 @@
+<?php   
+
+
+// PHP has 8 data types
+
+// STRING => chars surrounded by quotations
+
+$myStr = "my string";
+
+// INTEGER => whole numbers
+
+$myInt = 79;
+// FLOAT => decimal numbers
+
+$myFloat = 2.5;
+// BOOLEAN => true or false values
+
+$myBool = true;
+
+// ARRAY => indexed structure for holding more than one value
+ $myArray = [1,2,3,4,5];
+// OBJECT => a class
+
+// NULL => an empty variable
+
+$nullVar;
+
+// var_dump($nullVar); // => will output NULL
+
+// RESOURCE = > special variable holding a resource
+
+?>
